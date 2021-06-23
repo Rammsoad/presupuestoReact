1f5901ff977c3707e2ee156febc23814a11a209b
Rafael Ruiz <Rafael.Ruiz.Rodriguez@outlook.com>
2021-06-23 15:42:29 -0700

    +Se agregó la carga condicional del componente al seleccionar presupuesto

---
+++ src/components/Pregunta.js
@@ -0,0 +1,64 @@
+import React, {Fragment, useState} from 'react';
+import Error from './Error';
+
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+// definir el State
+
+const [cantidad, guardarCantidad] = useState(0);
+const [error, guardarError] = useState(false);
+
+//Función que lee el presupuesto
+
+const definirPresupuesto = e => {
+    guardarCantidad(parseInt(e.target.value,10))
+    }
+
+    //Submit para definir el presupuesto
+
+    const agregarPresupuesto = e => {
+            e.preventDefault();
+
+            //validar
+            if (cantidad < 1 || isNaN (cantidad)){
+                guardarError(true);
+                return;
+            }
+
+            //si pasa la validación
+                guardarError(false);
+                guardarPresupuesto(cantidad);
+                guardarRestante(cantidad);
+                actualizarPregunta(false);
+    }
+
+    return ( 
+
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+
+            {error ? <Error mensaje="El Presupuesto es Incorrecto"/> : null}
+
+            <form
+            onSubmit = {agregarPresupuesto}
+            >
+                <input
+                type = "number"
+                className = "u-full-width"
+                placeholder = "Coloca tu presupuesto"
+                onChange = {definirPresupuesto}
+                />
+
+                <input
+                type= "submit"
+                className = "button-primary u-full-width"
+                placeholder = "Definir presupuesto"
+                
+                />
+            </form>
+        </Fragment>
+
+     );
+}
+ 
+export default Pregunta;
